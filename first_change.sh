f060638afb956fba4e3c25cbf84c70bb27c822fa
Sapphire9898 <yueyangzou@gmail.com>
2017-03-29 21:46:14 -0700

    make the first change
    make the second change
    
    2'+
    
    1+

---
+++ first_change.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if true; then 
+    echo "Hello World for the first time!"
+fi
+if true; then 
+    echo "Hello World for the second time!"
+fi
+if true; then 
+    echo "Hello World for the fourth time!"
+    echo "Hello World for the third time!"
+fi
